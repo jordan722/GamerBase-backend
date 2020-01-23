79d54391b94e6781b7fc85a3d4c723c70650552c
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-23 17:05:52 -0500

    added user to post object

--- controllers/post.js
+++ controllers/post.js
@@ -1,16 +1,16 @@
-const { Post } = require("../database/models");
+const { User, Post } = require("../database/models");
 
 const postController = {
 	getAllPosts: getAllPosts,
-  addPost: addPost,
-  getPost: getPost,
-  updatePost: updatePost
+	addPost: addPost,
+	getPost: getPost,
+	updatePost: updatePost
 };
 
 // GET api/posts
 async function getAllPosts(req, res, next) {
 	try {
-		const posts = await Post.findAll();
+		const posts = await Post.findAll({ include: [User] });
 		res.status(200).json(posts);
 		console.log("attempting to find posts");
 	} catch (err) {
@@ -20,19 +20,19 @@ async function getAllPosts(req, res, next) {
 
 // POST api/posts
 async function addPost(req, res, next) {
-  try {
-    const newPost = await Post.create(req.body);
+	try {
+		const newPost = await Post.create(req.body);
 		console.log("looking for post");
-    res.status(201).json(newPost);
-  } catch (err) {
-    console.log(err);
-  }
+		res.status(201).json(newPost);
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 // GET api/post
-async function getPost(req, res, next){
-  try {
-		let post = await Post.findById(req.params.id);
+async function getPost(req, res, next) {
+	try {
+		let post = await Post.findById(req.params.id, { include: [User] });
 		if (post) {
 			res.json(post);
 		} else {
@@ -43,10 +43,10 @@ async function getPost(req, res, next){
 	}
 }
 
-async function updatePost(req,res,next){
-  try {
-		console.log('tried to update post')
-		console.log(req.body)
+async function updatePost(req, res, next) {
+	try {
+		console.log("tried to update post");
+		console.log(req.body);
 		let updatedPostInfo = await Post.update(req.body, {
 			where: { id: req.params.id },
 			returning: true,
@@ -57,7 +57,5 @@ async function updatePost(req,res,next){
 		next(err);
 	}
 }
-
-
 
 module.exports = postController;
